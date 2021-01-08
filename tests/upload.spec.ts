e29446a099c78db2aa16925c41fc930f098111c4
tarcisiocorte <tarcisiocorte@gmail.com>
2021-01-08 06:58:02 -0300

    test: return 400 if no userKey is provided

--- tests/upload.spec.ts
+++ tests/upload.spec.ts
@@ -6,7 +6,7 @@ describe('Upload Controller', () => {
     const httpRequest = {
       body: {
         companyKey: 'a_company_key',
-        userKey: 'an_use_key'
+        userKey: 'an_user_key'
       }
     }
     const httpResponse = sut.handle(httpRequest)
@@ -25,5 +25,18 @@ describe('Upload Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('companyKey'))
+  })
+
+  test('should return 400 if no userKey is provided', () => {
+    const sut = new UploadController()
+    const httpRequest = {
+      body: {
+        companyKey: 'a_company_key',
+        image: 'any_image'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('userKey'))
   })
 })
