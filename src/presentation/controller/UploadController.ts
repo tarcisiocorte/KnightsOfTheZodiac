ea060a104a1ad7a72e816d285b3319e87339a29b
Tarcisio Corte <tarcisiocorte@gmail.com>
2021-01-16 00:53:02 -0300

    refactor: add apiresult in the controller

--- src/presentation/controller/UploadController.ts
+++ src/presentation/controller/UploadController.ts
@@ -1,37 +1,22 @@
-import { HttpRequest, HttpResponse } from '../web'
+import { badRequest, HttpRequest, HttpResponse, ok } from '../web'
 
 export class UploadController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.companyKey) {
-      return {
-        statusCode: 400,
-        body: new Error('companyKey')
-      }
+      return badRequest(new Error('companyKey'))
     }
     if (!httpRequest.body.image) {
-      return {
-        statusCode: 400,
-        body: new Error('image')
-      }
+      return badRequest(new Error('image'))
     }
 
     if (!httpRequest.body.userKey) {
-      return {
-        statusCode: 400,
-        body: new Error('userKey')
-      }
+      return badRequest(new Error('userKey'))
     }
 
     if (!httpRequest.body.image.includes('.jpg', '.png')) {
-      return {
-        statusCode: 400,
-        body: new Error('The image has not the correct format')
-      }
+      return badRequest(new Error('The image has not the correct format'))
     }
 
-    return {
-      statusCode: 200,
-      body: httpRequest.body
-    }
+    return ok(httpRequest.body)
   }
 }
--- src/presentation/helpers/http-helper.ts
+++
@@ -1,6 +0,0 @@
-import { HttpResponse } from '../web/IHttpResponse'
-
-export const badRequest = (error: Error): HttpResponse => ({
-  statusCode: 400,
-  body: error
-})
