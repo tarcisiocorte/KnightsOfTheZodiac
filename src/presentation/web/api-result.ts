6a6ff3ef010d478139b07b98a74fb6bd8ca16bdd
Tarcisio Costa <tarcisiocorte@gmail.com>
2021-02-02 09:49:59 -0300

    chore: update ap-result to show only used statusCode

--- src/presentation/web/api-result.ts
+++ src/presentation/web/api-result.ts
@@ -5,11 +5,6 @@ export const badRequest = (error: Error): HttpResponse => ({
   body: error
 })
 
-export const forbidden = (error: Error): HttpResponse => ({
-  statusCode: 403,
-  body: error
-})
-
 /**
 export const unauthorized = (): HttpResponse => ({
   statusCode: 401,
@@ -25,9 +20,4 @@ export const serverError = (error: Error): HttpResponse => ({
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
-})
-
-export const noContent = (): HttpResponse => ({
-  statusCode: 204,
-  body: null
 })
